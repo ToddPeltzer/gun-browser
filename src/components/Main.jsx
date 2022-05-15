3367fde036bbde004c64a092819f3b526cf383c9
toddpeltzer <toddpeltzer14@gmail.com>
2022-05-15 08:49:07 -0600

    first commit after setting main

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+
+function Main(props) {
+
+
+    return (
+        <div>
+        
+            Main Page Test
+
+        </div>
+    );
+}
+
+export default Main;
